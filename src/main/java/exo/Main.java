e3fddbfeb68b1344efd62fbe5c021f2a4e805abe
VAUBAN_TEMP\Administrateur <anastasiia.penkina@gmail.com>
2023-06-15 11:58:12 +0200

    update clas Transformer

--- src/main/java/exo/Main.java
+++ src/main/java/exo/Main.java
@@ -1,14 +1,14 @@
 package exo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class Main {
 	public static void main(String[] args) {
-		Logger logger = LoggerFactory.getLogger(Main.class);
+		
 		
 		String message = "This is just a message to {name} who lives at {address} and works at {company}, you can call on {phone}";
-
+		
+		
 		Transformer transformer = new Transformer();
 		// Transformer georgeTransformer = new Transformer("name", "address", "phone",
 		// "company");
@@ -17,11 +17,10 @@ public class Main {
 		transformer.addToken("{address}", "78, rue Will Smith");
 		transformer.addToken("{phone}", "03 54 87 69 88");
 		transformer.addToken("{company}", "Amazon");
-		System.out.println(transformer.toString());
+		//System.out.println(transformer.toString());
 
-		logger.debug("Debug log message");
-        logger.info("Info log message");
-        logger.error("Error log message");
-        logger.warn("Warn log message");
+		
+        
+       
 	}
 }
--- src/main/java/exo/Transformer.java
+++ src/main/java/exo/Transformer.java
@@ -1,9 +1,13 @@
 package exo;
 
 import java.util.HashMap;
+
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Transform a text by replacing a set of tokens by pre-defined values: Example:
@@ -13,7 +17,8 @@ import java.util.regex.Pattern;
  **/
 
 public class Transformer {
-
+	
+	
 	private HashMap<String, String> tokens = new HashMap<String, String>();
 
 	public void addToken(String token, String value) {
@@ -47,9 +52,13 @@ public class Transformer {
 	}
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(Main.class);
+	
 		String message = "This is just a message to {name} who lives at {address} and works at {company}, you can call on {phone}";
-
+        logger.info(message);
 		Transformer transformer = new Transformer();
+		
+		
 		// Transformer georgeTransformer = new Transformer("name", "address", "phone",
 		// "company");
 
@@ -59,6 +68,12 @@ public class Transformer {
 		transformer.addToken("company", "Amazon");
 		System.out.println(transformer.toString());
 		System.out.println(transformer.transform(message));
-	}
 
+		
+		logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
+        logger.warn("Warn log message");
+
+}
 }
